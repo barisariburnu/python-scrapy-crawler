a4fd49dfa7bd602ee50db2e9930b671f916fce18
barisariburnu <barisariburnu@gmail.com>
2022-06-06 16:53:25 +0300

    Fix mongo url

--- udemy/pipelines.py
+++ udemy/pipelines.py
@@ -6,9 +6,7 @@ from udemy.settings import MONGO_USERNAME, MONGO_PASSWORD
 from pymongo import MongoClient
 
 client = MongoClient(
-    f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@ac-dczxfng-shard-00-00.latzb7l.mongodb.net:27017,
-    ac-dczxfng-shard-00-01.latzb7l.mongodb.net:27017,ac-dczxfng-shard-00-02.latzb7l.mongodb.net:27017/
-    ?ssl=true&replicaSet=atlas-bwqagz-shard-0&authSource=admin&retryWrites=true&w=majority"
+    f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@ac-dczxfng-shard-00-00.latzb7l.mongodb.net:27017,ac-dczxfng-shard-00-01.latzb7l.mongodb.net:27017,ac-dczxfng-shard-00-02.latzb7l.mongodb.net:27017/?ssl=true&replicaSet=atlas-bwqagz-shard-0&authSource=admin&retryWrites=true&w=majority"
 )
 db = client.udemy
 logger = logging.getLogger(__name__)
