6013141c4376cff4a28bd1ad0be4010027e7be09
barisariburnu <barisariburnu@gmail.com>
2022-06-06 16:53:40 +0300

    Add service.sh

---
+++ service.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /home/ubuntu/python-scrapy-crawler
+
+source /home/ubuntu/python-scrapy-crawler/venv3/bin/activate
+
+python3 /home/ubuntu/python-scrapy-crawler/venv3/bin/scrapy crawl udemy
