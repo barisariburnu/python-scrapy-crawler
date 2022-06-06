61c74b94a51955b05f78f98f52dbb1c08932a3f7
barisariburnu <barisariburnu@gmail.com>
2022-06-06 23:04:56 +0300

    Update udemy api params

--- udemy/settings.py
+++ udemy/settings.py
@@ -20,7 +20,7 @@ DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
 
 BASE_URL = f'https://www.udemy.com/api-2.0'
 ALL_COURSE_URL = f'/discovery-units/all_courses'
-PARAMS = 'page_size=60&lang=en&sort=newest&source_page=category_page&locale=en_US&navigation_locale=en_US&sos=pc&fl=cat'
+PARAMS = 'page_size=60&lang=en&sort=newest&source_page=category_page&locale=en_US&price=price-free&navigation_locale=en_US&sos=pc&fl=cat'
 CATEGORY_IDs = ['268', '269', '273', '274', '276', '278', '288', '290', '292', '294', '296', '300', '328']
 MONGO_USERNAME = 'barisariburnu'
 MONGO_PASSWORD = 'barisariburnu'
